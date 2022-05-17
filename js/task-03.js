32a0bc9417ae8edcd365819d79e894c8d9345889
Marek Kalus <marektg2@gmail.com>
2022-05-17 14:16:43 +0200

    task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,12 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const list = document.querySelector(".gallery");
+
+const photo = images.map((image) => `<li><img src=${image.url} alt=${image.alt} width = 100%></li>`).join("");
+
+list.insertAdjacentHTML("beforeend", photo);
+list.style.display = "flex";
+list.style.flexDirection = "column";
+list.style.listStyleType = "none"; 
