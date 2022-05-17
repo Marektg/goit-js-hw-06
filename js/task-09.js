0e8c263b0ae5706cc841b34f4b43bba0775f0456
Marek Kalus <marektg2@gmail.com>
2022-05-17 22:57:36 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector(".change-color");
+const strona = document.querySelector("body");
+const span = document.querySelector(".color");
+const zmiana = () => {
+  strona.style.backgroundColor = getRandomHexColor();
+  span.innerHTML = `${strona.style.backgroundColor}`;
+
+}
+btn.addEventListener("click", zmiana);
